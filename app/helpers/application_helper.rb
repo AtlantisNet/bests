d2ec40eec9c78be78c120d30aa1fb6568eaf4ca5
Manoel Estevam <atlantisnet@outlook.com.br>
2017-01-23 15:47:32 -0300

    Adicionado um full_title ao helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  #Retorna o título completo em uma base por página
+  def full_title(page_title = '')
+    base_title = "Bets bolão do brasieirão"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,7 +5,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Bets bolão do brasieirão"
+    assert_select "title", "Bets bolão do brasieirão"
   end
 
   test "should get ajuda" do
